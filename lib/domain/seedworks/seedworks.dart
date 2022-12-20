dbdd98ea5479cbf286d54dd88d320f6f054fa47d
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-12-20 11:29:11 -0300

    ;

--- lib/domain/seedworks/seedworks.dart
+++ lib/domain/seedworks/seedworks.dart
@@ -1,6 +1,7 @@
+export './searchable_repository/search_input.dart';
+export './searchable_repository/search_order.dart';
 export 'entity.dart';
 export 'generic_repository_interface.dart';
 export 'repository_interface.dart';
 export 'searchable_repository/searchable_repository_interface.dart';
-export './searchable_repository/search_input.dart';
-export './searchable_repository/search_order.dart';
+export 'usecase.dart';
