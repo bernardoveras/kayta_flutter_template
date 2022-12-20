748fc24dfa4863b6fd790e1946dc6a28054f0bba
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-12-20 15:49:59 -0300

    docs

--- lib/infra/seedworks/entity_adapter.dart
+++ lib/infra/seedworks/entity_adapter.dart
@@ -1,4 +1,5 @@
 import '../../domain/seedworks/seedworks.dart';
 import 'seedworks.dart';
 
+/// Seedwork for entity adapters.
 abstract class EntityAdapter<TEntity extends Entity> extends Adapter {}
