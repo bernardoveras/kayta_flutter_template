31d5d05a1f25b31bafaf9f4b80a8d4f59cd1d411
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-12-20 16:32:44 -0300

    Add colors

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+/// A utility class that holds constants for colors used values
+/// throughout the entire app.
+/// 
+/// This class has no constructor and all methods and variables are `static`.
+class AppColors {
+  const AppColors._();
+
+  static const Color primaryColor = Color(0xFF1365FE);
+
+  static const Color primaryLighterColor = Color(0xFFDCECF3);
+
+  static const Color primaryLightColor = Color(0xFF83BBED);
+
+  static const Color primaryMediumColor = Color(0xFF1452C7);
+
+  static const Color primaryDarkColor = Color(0xFF123F92);
+
+  static const Color primaryDarkerColor = Color(0xFF0E2A5F);
+
+  static const Color secondaryColor = Color(0xFF4B4B4D);
+
+  static const Color secondaryLightColor = Color(0xFFE6E7E8);
+
+  static const Color secondaryMediumColor = Color(0xFFBDBFC1);
+
+  static const Color secondaryDarkColor = Color(0xFF96999B);
+
+  static const Color highlightColor = Color(0xFFFF8836);
+
+  static const Color highlightLightColor = Color(0xFFFFD9B4);
+
+  static const Color highlightMediumColor = Color(0xFFFFAF6C);
+
+  static const Color highlightDarkColor = Color(0xFFA86130);
+
+  static const Color positiveColor = Color(0xFF2BB670);
+
+  static const Color positiveLightColor = Color(0xFFDEF6EA);
+
+  static const Color positiveMediumColor = Color(0xFF82D8AD);
+
+  static const Color positiveDarkColor = Color(0xFF228352);
+
+  static const Color negativeColor = Color(0xFFEB3E3D);
+
+  static const Color negativeLightColor = Color(0xFFFEE0E0);
+
+  static const Color negativeMediumColor = Color(0xFFF47271);
+
+  static const Color negativeDarkColor = Color(0xFFA93130);
+
+  static const Color warningColor = Color(0xFFEEC226);
+
+  static const Color warningLightColor = Color(0xFFFFF5D1);
+
+  static const Color warningMediumColor = Color(0xFFF9DD78);
+
+  static const Color warningDarkColor = Color(0xFFC4A127);
+
+  static const Color lowNeutralColor = Color(0xFF000000);
+
+  static const Color lowNeutralLightColor = Color(0xFFA0A3A8);
+
+  static const Color lowNeutralMediumColor = Color(0xFF52575B);
+
+  static const Color lowNeutralDarkColor = Color(0xFF25292B);
+
+  static const Color highNeutralColor = Color(0xFFFFFFFF);
+
+  static const Color highNeutralLightColor = Color(0xFFF9F9F9);
+
+  static const Color highNeutralMediumColor = Color(0xFFF2F2F2);
+
+  static const Color highNeutralDarkColor = Color(0xFFCBCCCF);
+}
